7b9141a8ebc6bfb6a38a55d7eb9d81f2f7b60070
dduoming <dltndudvldzm@naver.com>
2021-12-23 18:34:15 +0900

    12/23 라인 나잡아봐라 문제 리팩토링

--- codingtest12/LINE_1.py
+++ codingtest12/LINE_1.py
@@ -1,9 +1,8 @@
 #2019 상반기 LINE 인턴 채용 코딩테스트
 from collections import deque
-c=int(input())
+now_c=int(input())
 b=int(input())
-conny=[]
-conny.append(c)
+
 time=0
 brown=deque()
 brown.append((0,b))
@@ -11,43 +10,32 @@ visited = [{} for _ in range(200001)]
 
 conny_d=0#conny가 움직이는 거리
 flag=0
-while True:
+while now_c<200000:
+    now_c+=time
 
-    now_c=conny.pop()
-
-    if now_c>200000:
+# c의 위치 == b의 위치 비교 후 종료
+    if time in visited[now_c]:
         break
 
-    while brown:
+    for i in range(0, len(brown)):
         now_btime,now_b=brown.popleft()
 
-        if time!=now_btime:
-            brown.appendleft((now_btime,now_b))
-            break
-        if now_b==now_c:
-            flag=1
-            break
+
+        if  0<now_b*2 and now_b*2<=200000 and now_btime+1 not in visited[now_b*2]:
+            brown.append((now_btime+1,now_b*2))
+            visited[now_b *2][now_btime+1] = True
+
+        if  0<now_b+1 and now_b + 1 <= 200000  and now_btime+1 not in visited[now_b+1]:
+            brown.append((now_btime + 1, now_b +1))
+            visited[now_b + 1][now_btime+1] = True
+
+        if 0 <= now_b -1  and now_b - 1 <= 200000 and  now_btime+1 not in visited[now_b-1]:
+            brown.append((now_btime + 1, now_b -1))
+            visited[now_b-1][now_btime+1] = True
 
 
-        else:
-            if 0<= now_b*2 and now_b*2<=200000 and now_btime not in visited[now_b*2]:
-                brown.append((now_btime+1,now_b*2))
-                visited[now_b *2][now_btime] = True
-            if 0 <= now_b + 1 and now_b + 1 <= 200000  and now_btime not in visited[now_b+1]:
-                brown.append((now_btime + 1, now_b +1))
-                visited[now_b + 1][now_btime] = True
-            if 0 <= now_b -1  and now_b -1 <= 200000 and  now_btime not in visited[now_b-1]:
-                brown.append((now_btime + 1, now_b -1))
-                visited[now_b-1][now_btime] = True
-
-    if flag==1:
-        break
 
     time+=1
-    conny_d+=1
-    conny.append(now_c+conny_d)
-
-
 
 
 print(time)
